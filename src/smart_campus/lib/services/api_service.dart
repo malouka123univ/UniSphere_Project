2e61bdaaa81943faa95815e38b692d47e0f44910
ma lak <malak.randji@univ-constantine2.dz>
2026-04-20 22:31:18 +0100

    docs(week3): add os concept documentation to api service

--- src/smart_campus/lib/services/api_service.dart
+++ src/smart_campus/lib/services/api_service.dart
@@ -2,14 +2,22 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiService {
+  //	Utilisation du protocole HTTP/REST
+  //Fake Online REST API(fake server)
   static const String baseUrl = 'https://jsonplaceholder.typicode.com';
 
   Future<List<dynamic>> fetchPosts() async {
+    //// Récupère la liste des publications (posts) depuis l'API de manière asynchrone.
+    // Envoie la requête et attend la réponse du serveur de manière asynchrone
     final response = await http
+        // Effectue un appel GET vers l'URL spécifiée (convertie en objet Uri)
+        //La méthode HTTP pour récupérer des données.
         .get(Uri.parse('$baseUrl/posts'))
+        //le temps exact pour app attender avant dire que  il ya un probleme de connex
         .timeout(const Duration(seconds: 10));
 
     if (response.statusCode == 200) {
+      //TRANSFRE LES DONNER VER LIST OU MAPPING PIR TRITER
       return jsonDecode(response.body);
     } else {
       throw Exception('HTTP ${response.statusCode}');
