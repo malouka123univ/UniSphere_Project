3211e0a4f1fe73eec8f6e1a7af1b0b8eaa6adee7
ma lak <malak.randji@univ-constantine2.dz>
2026-04-20 22:32:39 +0100

    feat(week3): add network connectivity check to bloc

--- src/smart_campus/lib/blocs/announcement_bloc.dart
+++ src/smart_campus/lib/blocs/announcement_bloc.dart
@@ -3,15 +3,20 @@ import 'announcement_event.dart';
 import 'announcement_state.dart';
 import '../usecases/get_announcements.dart';
 
+import '../network/network_info.dart';
+
 class AnnouncementBloc extends Bloc<AnnouncementEvent, AnnouncementState> {
   final GetAnnouncementsUseCase getAnnouncementsUseCase;
+  final NetworkInfo networkInfo;
 
-  AnnouncementBloc({required this.getAnnouncementsUseCase})
-    : super(const AnnouncementInitial()) {
-    on<LoadAnnouncements>(_onLoadAnnouncements);
+  AnnouncementBloc({
+    required this.getAnnouncementsUseCase,
+    required this.networkInfo,
+  }) : super(const AnnouncementInitial()) {
+    on<LoadAnnouncements>(_onLoad);
   }
 
-  Future<void> _onLoadAnnouncements(
+  Future<void> _onLoad(
     LoadAnnouncements event,
     Emitter<AnnouncementState> emit,
   ) async {
@@ -19,14 +24,16 @@ class AnnouncementBloc extends Bloc<AnnouncementEvent, AnnouncementState> {
 
     try {
       final announcements = await getAnnouncementsUseCase();
+      final isOnline = await networkInfo.isConnected;
 
-      emit(AnnouncementLoaded(announcements: announcements));
-    } catch (error) {
       emit(
-        const AnnouncementError(
-          message: 'Ad loading failed. Please verify your connection',
+        AnnouncementLoaded(
+          announcements: announcements,
+          isFromCache: !isOnline,
         ),
       );
+    } catch (error) {
+      emit(const AnnouncementError(message: 'Error in loading'));
     }
   }
 }
