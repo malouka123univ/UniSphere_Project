934abb4d976ac025cf34390cd7f419d6a2f1af1d
ma lak <malak.randji@univ-constantine2.dz>
2026-04-20 21:50:22 +0100

    week2:Add comment of part 2

--- src/smart_campus/lib/app.dart
+++ src/smart_campus/lib/app.dart
@@ -16,19 +16,36 @@ class SmartCampusApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // --- ÉTAPE 1 : Instanciation des couches (Dependency Injection) ---
+
+    // Initialisation du service bas niveau (Réseau)
     final apiService = ApiService();
+
+    // Liaison de la couche Data : on passe le service au dépôt
     final repository = AnnouncementRepositoryImpl(apiService: apiService);
+
+    // Liaison de la couche Domain : on passe le dépôt au cas d'utilisation
     final getAnnouncementsUseCase = GetAnnouncementsUseCase(
       repository: repository,
     );
 
+    // --- ÉTAPE 2 : Fournir le BLoC à l'application ---
+
     return BlocProvider(
+      // 'create' est une méthode de fabrique (factory) lancée de manière paresseuse (lazy).
+      // Elle instancie le BLoC au moment où l'application en a besoin.
       create: (_) =>
+          // Injection de dépendance : On transmet le Use Case au constructeur du Bloc.
+          // Cela lie officiellement la couche Presentation à la couche Domain.
           AnnouncementBloc(getAnnouncementsUseCase: getAnnouncementsUseCase),
+
+      // MaterialApp définit la configuration globale de l'UI
       child: MaterialApp(
         title: 'Smart Campus',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: true),
+
+        // La page d'accueil qui pourra consommer le BLoC
         home: const AnnouncementsPage(),
       ),
     );
--- src/smart_campus/lib/blocs/announcement_event.dart
+++ src/smart_campus/lib/blocs/announcement_event.dart
@@ -1,11 +1,16 @@
 import 'package:equatable/equatable.dart';
 
+// La classe de base pour tous les événements liés aux annonces
 abstract class AnnouncementEvent extends Equatable {
   const AnnouncementEvent();
+
+  // Equatable permet d'éviter de déclencher le même événement plusieurs fois par erreur
   @override
   List<Object?> get props => [];
 }
 
+// L'événement spécifique qui déclenche le chargement des données
+// On l'appelle quand l'utilisateur ouvre l'écran ou tire pour rafraîchir (Pull-to-refresh)
 class LoadAnnouncements extends AnnouncementEvent {
   const LoadAnnouncements();
 }
