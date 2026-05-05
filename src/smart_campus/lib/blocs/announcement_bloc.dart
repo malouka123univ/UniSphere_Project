e725e7b5302aa10af513a3a5f3c0b1ebdec3a244
DjekhrabChaima <chaima.djekhrab@univ-constantine2.dz>
2026-05-05 17:00:39 +0100

    feature(announcements): add filtering announcement

--- src/smart_campus/lib/blocs/announcement_bloc.dart
+++ src/smart_campus/lib/blocs/announcement_bloc.dart
@@ -7,43 +7,40 @@ import '../entities/announcement_entity.dart'; // Import ajouté
 
 class AnnouncementBloc extends Bloc<AnnouncementEvent, AnnouncementState> {
   final GetAnnouncementsUseCase getAnnouncementsUseCase;
-  final NetworkInfo networkInfo;
 
-  AnnouncementBloc({
-    required this.getAnnouncementsUseCase,
-    required this.networkInfo,
-  }) : super(const AnnouncementInitial()) {
-
-    // Enregistrement des gestionnaires d'événements
-    on<LoadAnnouncements>(_onLoad);
-    on<AddAnnouncement>(_onAddAnnouncement); // Déplacé ici
+  AnnouncementBloc({required this.getAnnouncementsUseCase})
+    : super(const AnnouncementInitial()) {
+    on<LoadAnnouncements>(_onLoadAnnouncements);
+    on<FilterChanged>(_onFilterChanged);
   }
 
-  Future<void> _onLoad(LoadAnnouncements event, Emitter<AnnouncementState> emit) async {
+  Future<void> _onLoadAnnouncements(
+    LoadAnnouncements event,
+    Emitter<AnnouncementState> emit,
+  ) async {
     emit(const AnnouncementLoading());
     try {
       final announcements = await getAnnouncementsUseCase();
-      final isOnline = await networkInfo.isConnected;
-      emit(AnnouncementLoaded(
-        announcements: announcements,
-        isFromCache: !isOnline,
-      ));
+      emit(AnnouncementLoaded(announcements: announcements));
     } catch (error) {
-      emit(const AnnouncementError(message: 'Error in loading'));
+      emit(
+        const AnnouncementError(
+          message:
+              'Failed to load announcements. Please check your connection.',
+        ),
+      );
     }
   }
 
-  void _onAddAnnouncement(AddAnnouncement event, Emitter<AnnouncementState> emit) {
+  void _onFilterChanged(FilterChanged event, Emitter<AnnouncementState> emit) {
     if (state is AnnouncementLoaded) {
       final currentState = state as AnnouncementLoaded;
-      // Création d'une nouvelle liste avec l'élément ajouté au début
-      final updatedList = List<AnnouncementEntity>.from(currentState.announcements)
-        ..insert(0, event.announcement);
-
-      emit(AnnouncementLoaded(
-        announcements: updatedList,
-        isFromCache: currentState.isFromCache,
-      ));
+      emit(
+        AnnouncementLoaded(
+          announcements: currentState.announcements,
+          selectedFilter: event.filter,
+        ),
+      );
     }
   }
 }
