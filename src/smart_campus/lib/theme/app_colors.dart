4dcd4b6fd454c63be2796840f568c19b9cc3158f
DjekhrabChaima <chaima.djekhrab@univ-constantine2.dz>
2026-05-05 16:47:03 +0100

    UI: add app colors class

---
+++ src/smart_campus/lib/theme/app_colors.dart
@@ -0,0 +1,41 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color primary = Color(0xFF00236F);
+  static const Color onPrimary = Color(0xFFFFFFFF);
+  static const Color primaryContainer = Color(0xFF1E3A8A);
+  static const Color onPrimaryContainer = Color(0xFF90A8FF);
+  static const Color primaryFixed = Color(0xFFDCE1FF);
+  static const Color primaryFixedDim = Color(0xFFB6C4FF);
+  static const Color secondary = Color(0xFF555D7E);
+  static const Color onSecondary = Color(0xFFFFFFFF);
+  static const Color secondaryContainer = Color(0xFFD0D8FF);
+  static const Color onSecondaryContainer = Color(0xFF555E7F);
+  static const Color tertiary = Color(0xFF3E2400);
+  static const Color onTertiary = Color(0xFFFFFFFF);
+  static const Color tertiaryContainer = Color(0xFF5C3800);
+  static const Color onTertiaryContainer = Color(0xFFEF9900);
+  static const Color tertiaryFixed = Color(0xFFFFDDB8);
+  static const Color tertiaryFixedDim = Color(0xFFFFB95F);
+  static const Color error = Color(0xFFBA1A1A);
+  static const Color onError = Color(0xFFFFFFFF);
+  static const Color errorContainer = Color(0xFFFFDAD6);
+  static const Color onErrorContainer = Color(0xFF93000A);
+  static const Color surface = Color(0xFFFAF8FF);
+  static const Color onSurface = Color(0xFF1A1B21);
+  static const Color onSurfaceVariant = Color(0xFF444651);
+  static const Color surfaceVariant = Color(0xFFE3E1E9);
+  static const Color surfaceContainerLowest = Color(0xFFFFFFFF);
+  static const Color surfaceContainerLow = Color(0xFFF4F3FA);
+  static const Color surfaceContainer = Color(0xFFEEEDF4);
+  static const Color surfaceContainerHigh = Color(0xFFE9E7EF);
+  static const Color surfaceContainerHighest = Color(0xFFE3E1E9);
+  static const Color outline = Color(0xFF757682);
+  static const Color outlineVariant = Color(0xFFC5C5D3);
+  static const Color inverseSurface = Color(0xFF2F3036);
+  static const Color inverseOnSurface = Color(0xFFF1F0F7);
+  static const Color inversePrimary = Color(0xFFB6C4FF);
+  static const Color surfaceTint = Color(0xFF4059AA);
+  static const Color surfaceBright = Color(0xFFFAF8FF);
+  static const Color surfaceDim = Color(0xFFDAD9E1);
+}
